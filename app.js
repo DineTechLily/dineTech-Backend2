2c440de79ccb130157e558faee107ad8208a1368
souhaksou <98225683+souhaksou@users.noreply.github.com>
2023-04-21 11:04:33 +0800

    app
    app

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var adminRouter = require('./routes/admin');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/admin', adminRouter);
+
+module.exports = app;
