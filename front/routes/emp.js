beb89eb6223c97467c395cb07ec0246c0f6fbb0b
samuel3wang <yuanyuan030303@gmail.com>
2023-04-22 23:02:44 +0800

    can register the account

--- front/routes/emp.js
+++ front/routes/emp.js
@@ -8,19 +8,44 @@ const Account = require("../models/empModels");
 // message
 const loginFailedError = { message: 'Login Failed, Please Try Again!' };
 
+// register just for testing purposes
+router.post('/register', async function (req, res) {
+  password = await bcrypt.hash(req.body.password, 12);
+  console.log(req.body.emp_id+req.body.emp_id)
+  const newAccount = await Account.create({
+    account : req.body.company_id + req.body.emp_id,
+    password: password
+  });
+  res.status(200).json({
+    status: 'success',
+    post: newAccount
+  });
+});
+
 router.post('/login', async function (req, res, next) {
+
+  // corresponds to the request
   const { company_id, emp_id, password } = req.body;
   const account = req.body.company_id + req.body.emp_id;
+  
+  // cannot leave the empty space
   if (!emp_id || !password || !company_id) {
     res.status(401).json({
       message: 'Cannot Leave Empty Blank'
     });
   }
+  // get user account and password
   const user = await Account.findOne({account: account}).select('+password')
+
+  // if the user existed
   if (!user) {
     res.status(401).json(loginFailedError);
+
+  // if password is incorrect
   } else if (!bcrypt.compare(req.body.password, user.password)) {
     res.status(401).json(loginFailedError);
+  
+  // if the password is correct, generate the jwt token
   } else {
     const token = jwt.sign({account},"SecrEt",{
       expiresIn: 1
@@ -46,16 +71,3 @@ module.exports = router;
 //   });
 // });
 
-// // register 
-// router.post('/register', async function (req, res) {
-//   password = await bcrypt.hash(req.body.password, 12);
-//   console.log(req.body.emp_id+req.body.emp_id)
-//   const newAccount = await Account.create({
-//     account : req.body.company_id + req.body.emp_id,
-//     password: password
-//   });
-//   res.status(200).json({
-//     status: 'success',
-//     post: newAccount
-//   });
-// });
