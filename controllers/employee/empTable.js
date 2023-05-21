4f0feb88d3def5abe9c8873721bcbf9e3270cc0b
samuel3wang <yuanyuan030303@gmail.com>
2023-05-22 00:42:13 +0800

    emp table

---
+++ controllers/employee/empTable.js
@@ -0,0 +1,41 @@
+const Guest = require("../../models/custGuestModel");
+const Order = require("../../models/custOrderModel");
+
+const table = {
+  async getTable(_, res) {
+    try {
+      const data = await Guest.find();
+      res.status(200).json({
+        success: true,
+        data: data,
+      });
+    } catch (error) {
+      res.status(400).json({
+        message: error,
+      });
+    }
+  },
+  async getOrder(req, res) {
+    const table_id = req.params.table_id;
+
+    try {
+      const all_order = await Guest.find({table_id: table_id});
+      const order_list = all_order[0].order_id;
+      const data = [];
+
+      for (let i = 0; i < order_list.length; i++) {
+        let order = await Order.find({order_id: order_list[i]})
+        data.push(order)
+      }
+      res.status(200).json({
+        success: true,
+        data: data,
+      });
+    } catch (error) {
+      res.status(400).json({
+        message: error,
+      });
+    }
+  },
+};
+module.exports = table;
--- routes/employee/emp.js
+++ routes/employee/emp.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const LoginControllers = require("../../controllers/employee/empLogin");
-// const TableControllers = require("../../controllers/employee/empTable");
+const TableControllers = require("../../controllers/employee/empTable");
 // const DoneControllers = require("../../controllers/employee/empDone");
 // const OrderControllers = require("../../controllers/employee/empOrder");
 // const CheckControllers = require("../../controllers/employee/empCheck");
 
 // register just for testing purposes
-router.post("/register",  LoginControllers.register);
-router.post("/login",     LoginControllers.login);
-router.post("/logout",    LoginControllers.logout);
+router.post("/register",      LoginControllers.register);
+router.post("/login",         LoginControllers.login);
+router.post("/logout",        LoginControllers.logout);
 
-// router.get("/table", TableControllers.);
-// router.get("/table/:table_id", TableControllers.);
+router.get("/table",           TableControllers.getTable);
+router.get("/table/:table_id", TableControllers.getOrder);
 
 // router.get("/done/:table_id", DoneControllers.);
 
