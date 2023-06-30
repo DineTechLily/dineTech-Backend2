20419f6934353bc3f39eeea224468ca49e33c90d
samuel3wang <yuanyuan030303@gmail.com>
2023-06-30 23:55:12 +0800

    ws

--- controllers/customer/custOrder.js
+++ controllers/customer/custOrder.js
@@ -5,8 +5,8 @@ const Guest = require("../../models/custGuestModel");
 const eOrder = require("../../models/empOrderModel");
 const Item = require("../../models/itemModel")
 const WebSocket = require('ws');
-// const ws = new WebSocket("wss://dinetech-host2.onrender.com");
-const ws = new WebSocket('ws://localhost:3006');
+const ws = new WebSocket("wss://dinetech-host2.onrender.com");
+// const ws = new WebSocket('ws://localhost:3006');
 
 ws.on('open', () => {
   console.log('connect to server')
@@ -71,7 +71,7 @@ const order = {
       if(guestData){
         ws.send(JSON.stringify(guestData));
         console.log("send ws data")
-        
+
       }
 
       res.status(200).json({
